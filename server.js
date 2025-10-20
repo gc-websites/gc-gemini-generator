f3b9ef4386f2b051223e3608f1114b932f5f1ebf
Назар Івченко <nazary9911@gmail.com>
2025-10-20 14:30:43 +0300

    fix

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express';
 // import cors from 'cors';
 import dotenv from 'dotenv';
 import crypto from "crypto";
-import cron from 'node-cron';
-import {generateAndPost} from './functions.js';
+// import cron from 'node-cron';
+// import {generateAndPost} from './functions.js';
 import { generateImg, generateProduct, postToStrapi } from './functionsForProducts.js';
 
 const server = express();
@@ -30,47 +30,47 @@ server.use(express.json());
 // server.use(cors(corsOptions));
 
 
-let isRunning = false;
-cron.schedule('0 8,20 * * *', async () => {
-  if (isRunning) {
-    console.log('generateAndPost already running — skipping this run.');
-    return;
-  }
-  isRunning = true;
-  try {
-    console.log('Scheduled job start:', new Date().toISOString());
-    await generateAndPost();
-    console.log('Scheduled job end:', new Date().toISOString());
-  } catch (err) {
-    console.error('Scheduled job error:', err);
-  } finally {
-    isRunning = false;
-  }
-}, {
-  timezone: 'Europe/Kiev' // передаёте нужный IANA timezone
-});
-// server.post('/generate-post', async (req, res) => {
-//   generateAndPost();
-// });
-
-// server.post('/generate-posts', async (req, res) => {
-//   const {topics} = req.body;
-//   const results = [];
-//   for(const query of topics){
-//     try{
-//       console.log(`⚡ Начинаю обработку темы: "${query}"`);
-//       const result = await generateGlobalObj(query);
-//       const imageids = await generateImages(result);
-//       const resultToStrapiPost = await prepForPush(result, imageids);
-//       const isPostedToStrapi = await strapiPost(resultToStrapiPost);
-//       results.push({ query, status: 'ok', result: isPostedToStrapi });
-//     }catch(error){
-//       console.error(`❌ Ошибка при обработке "${query}"`, error);
-//       results.push({ query, status: 'error', error: error.message });
-//     }
+// let isRunning = false;
+// cron.schedule('0 8,20 * * *', async () => {
+//   if (isRunning) {
+//     console.log('generateAndPost already running — skipping this run.');
+//     return;
 //   }
-//   res.json(results);
+//   isRunning = true;
+//   try {
+//     console.log('Scheduled job start:', new Date().toISOString());
+//     await generateAndPost();
+//     console.log('Scheduled job end:', new Date().toISOString());
+//   } catch (err) {
+//     console.error('Scheduled job error:', err);
+//   } finally {
+//     isRunning = false;
+//   }
+// }, {
+//   timezone: 'Europe/Kiev' // передаёте нужный IANA timezone
 // });
+server.post('/generate-post', async (req, res) => {
+  generateAndPost();
+});
+
+server.post('/generate-posts', async (req, res) => {
+  const {topics} = req.body;
+  const results = [];
+  for(const query of topics){
+    try{
+      console.log(`⚡ Начинаю обработку темы: "${query}"`);
+      const result = await generateGlobalObj(query);
+      const imageids = await generateImages(result);
+      const resultToStrapiPost = await prepForPush(result, imageids);
+      const isPostedToStrapi = await strapiPost(resultToStrapiPost);
+      results.push({ query, status: 'ok', result: isPostedToStrapi });
+    }catch(error){
+      console.error(`❌ Ошибка при обработке "${query}"`, error);
+      results.push({ query, status: 'error', error: error.message });
+    }
+  }
+  res.json(results);
+});
 
 server.post('/generate-product', async (req, res) => {
   const {query, link} = req.body;
