80da74868546a814fbfa212920bd2f139e87a0a0
Назар Івченко <nazary9911@gmail.com>
2025-10-20 15:24:42 +0300

    fix

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import crypto from "crypto";
-// import cron from 'node-cron';
-// import {generateAndPost} from './functions.js';
+import cron from 'node-cron';
+import {generateAndPost} from './functions.js';
 import { generateImg, generateProduct, postToStrapi } from './functionsForProducts.js';
 
 const server = express();
@@ -16,61 +16,61 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const PIXEL_ID = process.env.PIXEL_ID;
 const PIXEL_TOKEN = process.env.PIXEL_TOKEN ;
 
-// const corsOptions = {
-// 	origin: [
-// 		'https://nice-advice.info',
-// 		'https://www.nice-advice.info',
-//     'http://localhost:5173'
-// 	],
-// 	credentials: true,
-// 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-// }
+const corsOptions = {
+	origin: [
+		'https://nice-advice.info',
+		'https://www.nice-advice.info',
+    'http://localhost:5173'
+	],
+	credentials: true,
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}
 
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 
 
-// let isRunning = false;
-// cron.schedule('0 8,20 * * *', async () => {
-//   if (isRunning) {
-//     console.log('generateAndPost already running — skipping this run.');
-//     return;
-//   }
-//   isRunning = true;
-//   try {
-//     console.log('Scheduled job start:', new Date().toISOString());
-//     await generateAndPost();
-//     console.log('Scheduled job end:', new Date().toISOString());
-//   } catch (err) {
-//     console.error('Scheduled job error:', err);
-//   } finally {
-//     isRunning = false;
-//   }
-// }, {
-//   timezone: 'Europe/Kiev' // передаёте нужный IANA timezone
-// });
+let isRunning = false;
+cron.schedule('0 8,20 * * *', async () => {
+  if (isRunning) {
+    console.log('generateAndPost already running — skipping this run.');
+    return;
+  }
+  isRunning = true;
+  try {
+    console.log('Scheduled job start:', new Date().toISOString());
+    await generateAndPost();
+    console.log('Scheduled job end:', new Date().toISOString());
+  } catch (err) {
+    console.error('Scheduled job error:', err);
+  } finally {
+    isRunning = false;
+  }
+}, {
+  timezone: 'Europe/Kiev'
+});
 server.post('/generate-post', async (req, res) => {
   generateAndPost();
 });
 
-server.post('/generate-posts', async (req, res) => {
-  const {topics} = req.body;
-  const results = [];
-  for(const query of topics){
-    try{
-      console.log(`⚡ Начинаю обработку темы: "${query}"`);
-      const result = await generateGlobalObj(query);
-      const imageids = await generateImages(result);
-      const resultToStrapiPost = await prepForPush(result, imageids);
-      const isPostedToStrapi = await strapiPost(resultToStrapiPost);
-      results.push({ query, status: 'ok', result: isPostedToStrapi });
-    }catch(error){
-      console.error(`❌ Ошибка при обработке "${query}"`, error);
-      results.push({ query, status: 'error', error: error.message });
-    }
-  }
-  res.json(results);
-});
+// server.post('/generate-posts', async (req, res) => {
+//   const {topics} = req.body;
+//   const results = [];
+//   for(const query of topics){
+//     try{
+//       console.log(`⚡ Начинаю обработку темы: "${query}"`);
+//       const result = await generateGlobalObj(query);
+//       const imageids = await generateImages(result);
+//       const resultToStrapiPost = await prepForPush(result, imageids);
+//       const isPostedToStrapi = await strapiPost(resultToStrapiPost);
+//       results.push({ query, status: 'ok', result: isPostedToStrapi });
+//     }catch(error){
+//       console.error(`❌ Ошибка при обработке "${query}"`, error);
+//       results.push({ query, status: 'error', error: error.message });
+//     }
+//   }
+//   res.json(results);
+// });
 
 server.post('/generate-product', async (req, res) => {
   const {query, link} = req.body;
