811b0a46e9e631a1390c3906c9f050b5706b5e59
Nazar <nazary9911@gmail.com>
2026-02-15 04:13:06 +0200

    fix

--- tgBot.js
+++ tgBot.js
@@ -4,9 +4,9 @@ import TelegramBot from "node-telegram-bot-api";
 export function createTelegramBot(token) {
   const bot = new TelegramBot(token, { polling: true });
 
-  bot.on("message", (msg) => {
-    bot.sendMessage(msg.chat.id, "Бот на связи 🚀");
-  });
+  // bot.on("message", (msg) => {
+  //   bot.sendMessage(msg.chat.id, "Бот на связи 🚀");
+  // });
 
   return bot;
 }
